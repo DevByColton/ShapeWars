55145f6dd02140827f5da566dc6ee4d187180f1d
Colton <developmentbycolton@outlook.com>
2025-12-11 05:56:46 -0600

    Release build updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
+#include <windows.h>
+#include <iostream>
 #include "GameRoot.h"
+
+void checkDebug()
+{
+#ifdef DEBUG_BUILD
+    std::cout << "Running in Debug mode." << std::endl;
+#elif defined(RELEASE_BUILD)
+    FreeConsole();
+#else
+    std::cout << "Running in an unknown build mode." << std::endl;
+#endif
+}
 
 int main()
 {
+    checkDebug();
     GameRoot::instance().load();
     GameRoot::instance().run();
     return 0;
